a5d6e4c5391780b348c10ad56ee8e66aac7f12e7
hikigg <mcpe951753@gmail.com>
2024-08-30 00:48:13 -0500

    Montaje del proyecto

---
+++ src/main/java/com/shoescompany/ShoesCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.shoescompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoesCompanyApplication.class, args);
+	}
+
+}
